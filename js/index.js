0b4f3126affa2109f9ee7e56a2734bc791756153
Fhilipe Monteiro <fhilipemonteiro@hotmail.com>
2022-12-04 22:20:55 -0300

    adicao funcionalidade de editar e excluir lembretes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-let usersDB = JSON.parse(localStorage.getItem('users'));
+const urlDB = 'users';
 
-let userLogado = JSON.parse(sessionStorage.getItem('usuarioLogado'));
+let usersDB = getUsersDB(urlDB);
 
-let indexUsuario = usersDB.findIndex(element => element.user === userLogado.user);
+let userLogado = getUserLogado();
+
+let indexUsuario = getIndexUser(usersDB, userLogado);
 
 innerHTML(userLogado.recados);
 
@@ -10,7 +12,7 @@ document.querySelector('#form-new-note').addEventListener('submit', event => {
 
   event.preventDefault();
 
-  document.querySelector('tbody').innerHTML = ``
+  document.querySelector('tbody').innerHTML = ``;
 
   const descricao = document.querySelector('#input-description').value;
   const detalhamento = document.querySelector('#input-detailing').value;
@@ -22,40 +24,44 @@ document.querySelector('#form-new-note').addEventListener('submit', event => {
   
   userLogado.recados.push(recado);
 
-  const recados = getRecados(userLogado);
-
+  
   usersDB[indexUsuario] = userLogado;
-
+  
   resetInput();
-
+  
   postData(usersDB, userLogado);
 
-  innerHTML(recados);
+  const recados = getRecados(userLogado);
 
+  innerHTML(recados);
+  
 });
 
 function resetInput() {
+
   document.querySelector('#input-description').value = '';
   document.querySelector('#input-detailing').value = '';
+
 };
 
 function innerHTML(element){
 
-  element.forEach( (element, index) => {
+  element.forEach( (element, id) => {
     
     document.querySelector('tbody').innerHTML += ` 
     <tr class="tr-card">
-      <td class="td-id">${index + 1}</td>
-      <td class="td-descricao">${element.descricao}</td>
-      <td class="td-detalhamento">${element.detalhamento}</td>
+      <td class="td-id">${id + 1}</td>
+      <td class="td-descricao" id="descricao${id}">${element.descricao}</td>
+      <td class="td-detalhamento" id="detalhamento${id}">${element.detalhamento}</td>
       <td class="td-acao">
-        <div class="div-btn-acao">
-          <div>
-            <input type="submit" class="btn-criar" value="CRIAR">
-          </div>
-          <div>
-            <input type="reset" class="btn-limpar" value="LIMPAR">
-          </div>
+          <div class="div-btn-acao">
+            <div id="btn-editar${id}">
+              <button class="btn-editar" onclick="editarRecado(${id})">EDITAR</button>
+            </div>
+
+            <div id="btn-excluir${id}">
+              <button class="btn-excluir" onclick="excluirRecado(${id})">EXCLUIR</button>
+            </div>
         </div>
       </td>
     </tr>`
@@ -65,14 +71,102 @@ function innerHTML(element){
 };
 
 function postData(usersDB, userLogado){
-
+  
   localStorage.setItem('users', JSON.stringify(usersDB));
   sessionStorage.setItem('usuarioLogado', JSON.stringify(userLogado));
-
+  
 };
 
 function getRecados(element){
-
-  return element.recados
+  
+  return element.recados;
 
 };
+
+function getUsersDB(url){
+  
+  return JSON.parse(localStorage.getItem(`${url}`));
+  
+};
+
+function getUserLogado() {
+  
+  return JSON.parse(sessionStorage.getItem('usuarioLogado'));
+  
+};
+
+function getIndexUser(usersDB, userLogado){
+  
+  return usersDB.findIndex(element => element.user === userLogado.user);
+  
+};
+
+function editarRecado(id) {
+
+  const recado = userLogado.recados[id];
+  
+  document.querySelector(`#descricao${id}`).innerHTML = `<input type="text" class="td-input-descricao" id="td-descricao${id}" autocomplete="off" placeholder="${recado.descricao}"></input>`;
+
+  document.querySelector(`#detalhamento${id}`).innerHTML = `<input type="text" class="td-input-detalhamento" id="td-detalhamento${id}" autocomplete="off" placeholder="${recado.detalhamento}"></input>`;
+
+  document.querySelector(`#btn-editar${id}`).innerHTML = `<button class="btn-editar" id="btn-editar${id}" onclick="salvarEdicaoRecado(${id})">SALVAR</button>`;
+
+  document.querySelector(`#btn-excluir${id}`).innerHTML = `<button class="btn-excluir" id="btn-excluir${id}" onclick="cancelar(${id})">CANCELAR</button>`;
+  
+};
+
+function salvarEdicaoRecado(id) {
+
+  const descricao = document.querySelector(`#td-descricao${id}`).value;
+  const detalhamento = document.querySelector(`#td-detalhamento${id}`).value;
+
+  const novoRecado = {
+    descricao: !descricao ? userLogado.recados[id].descricao : descricao,
+    detalhamento: !detalhamento ? userLogado.recados[id].detalhamento :detalhamento
+  };
+  
+  userLogado.recados.splice(id, 1, novoRecado);
+
+  usersDB[indexUsuario] = userLogado;
+    
+  postData(usersDB, userLogado);
+  
+  document.querySelector(`#descricao${id}`).innerHTML = `${novoRecado.descricao}`;
+  
+  document.querySelector(`#detalhamento${id}`).innerHTML = `${novoRecado.detalhamento}`;
+  
+  document.querySelector(`#btn-editar${id}`).innerHTML = `<button class="btn-editar" onclick="editarRecado(${id})">EDITAR</button>`;
+
+  document.querySelector(`#btn-excluir${id}`).innerHTML = `<button class="btn-excluir" onclick="excluirRecado(${id})">EXCLUIR</button>`
+
+};
+
+function cancelar(id) {
+  
+  const recado = userLogado.recados[id];
+
+  document.querySelector(`#descricao${id}`).innerHTML = `${recado.descricao}`;
+  
+  document.querySelector(`#detalhamento${id}`).innerHTML = `${recado.detalhamento}`;
+  
+  document.querySelector(`#btn-editar${id}`).innerHTML = `<button class="btn-editar" onclick="editarRecado(${id})">EDITAR</button>`;
+
+  document.querySelector(`#btn-excluir${id}`).innerHTML = `<button class="btn-excluir" onclick="excluirRecado(${id})">EXCLUIR</button>`
+
+};
+
+function excluirRecado(id) {
+  
+  userLogado.recados.splice(id,1);
+
+  usersDB[indexUsuario] = userLogado;
+
+  const recados = userLogado.recados;
+
+  postData(usersDB,userLogado);
+
+  document.querySelector('tbody').innerHTML = ``;
+
+  innerHTML(recados);
+
+}
